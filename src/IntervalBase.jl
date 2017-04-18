9815082c0024beffb7a879c26f35c781247666d2
David Sanders <dpsanders@gmail.com>
2017-04-18 12:38:04 -0500

    Exports

--- src/IntervalBase.jl
+++ src/IntervalBase.jl
@@ -2,6 +2,8 @@ module IntervalBase
 
 using Compat
 
+export AbstractInterval, ArithmeticInterval
+
 @compat abstract type AbstractInterval{T} end
 
 @compat abstract type ArithmeticInterval{T} <: Real end
