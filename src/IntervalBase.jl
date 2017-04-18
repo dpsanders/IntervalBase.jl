dd0eb7535be738402fd127081bf35db54614a385
David Sanders <dpsanders@gmail.com>
2017-04-18 12:34:25 -0500

    Abstract interval definitions

--- src/IntervalBase.jl
+++ src/IntervalBase.jl
@@ -1,5 +1,13 @@
 module IntervalBase
 
+using Compat
+
+@compat abstract type AbstractInterval{T} end
+
+@compat abstract type ArithmeticInterval{T} <: Real end
+
+
+
 # package code goes here
 
 end # module
